bd35353cde7cf9f02526c16e810c7c03545e15f6
benben-xu <104638365+benben-xu@users.noreply.github.com>
2024-04-04 12:51:29 +0800

    Add files via upload
    登录验证
    用户01
    密码123456

---
+++ vertify.js
@@ -0,0 +1,16 @@
+﻿
+function check(thisform) {
+ 
+	var name=document.getElementById("name").value; 
+	var pass=document.getElementById("pass").value;
+ 
+	if (name==="01" && pass==="123456") {  
+		alert("登录成功！");
+		window.document.f.action="index.html";  //此处设置登录后跳转页面
+		window.document.f.submit();
+	}
+	else{
+		alert("工号或密码错误！");
+	}
+ 
+}
